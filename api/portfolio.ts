1117ef77a534e5611c5ca09b09c4dd9b705b4074
agnes97 <janachaloupkova97@gmail.com>
2023-11-30 16:26:15 +0100

    🚑 chore: confirm which package causes Vercel build error

--- api/portfolio.ts
+++ api/portfolio.ts
@@ -1,28 +1,28 @@
 import { Application, Router } from "https://deno.land/x/oak@v12.6.1/mod.ts";
-import { drizzle } from 'npm:drizzle-orm/vercel-postgres';
-import { sql } from "npm:@vercel/postgres";
-import {
-  pgTable,
-  text,
-} from "npm:drizzle-orm/pg-core";
+// import { drizzle } from 'npm:drizzle-orm/vercel-postgres';
+import { sql } from "npm:@vercel/postgres"; // This causes build error!
+// import {
+//   pgTable,
+//   text,
+// } from "npm:drizzle-orm/pg-core";
 
 // Use this object to send drizzle queries to your DB
-export const db = drizzle(sql);
+// export const db = drizzle(sql);
 
 // Create a pgTable that maps to a table in your DB
-export const AccessTable = pgTable(
-  'Access',
-  {
-    id: text('id').primaryKey(),
-  },
-);
+// export const AccessTable = pgTable(
+//   'Access',
+//   {
+//     id: text('id').primaryKey(),
+//   },
+// );
 
 const router = new Router();
 
-router.get("/api/portfolio", async ({ response }) => {
-  const ids = await db.select().from(AccessTable);
-  response.body = ids;
-});
+// router.get("/api/portfolio", async ({ response }) => {
+//   const ids = await db.select().from(AccessTable);
+//   response.body = ids;
+// });
 
 const portfolioApp = new Application();
 portfolioApp.use(router.routes());
